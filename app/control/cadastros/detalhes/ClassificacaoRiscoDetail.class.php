613cf0dcdf0fd0ea03cad69075ce32c6596959d4
pedrohenrykes <pedrohenrykes@gmail.com>
2017-10-20 19:03:22 -0200

    Correções na classe

--- app/control/cadastros/detalhes/ClassificacaoRiscoDetail.class.php
+++ app/control/cadastros/detalhes/ClassificacaoRiscoDetail.class.php
@@ -60,6 +60,7 @@ class ClassificacaoRiscoDetail extends TPage
 
         $fk = filter_input( INPUT_GET, "fk" );
         $did = filter_input( INPUT_GET, "did" );
+        $page = filter_input( INPUT_GET, "page" );
 
         try {
 
@@ -68,7 +69,7 @@ class ClassificacaoRiscoDetail extends TPage
             $bau = new BauRecord( $fk );
             $paciente = new PacienteRecord( $did );
 
-            if( isset( $bau ) && isset( $paciente ) ) {
+            if( !empty( $bau ) && !empty( $paciente ) ) {
 
                 $bau_id->setValue( $bau->id );
                 $paciente_id->setValue( $paciente->id );
@@ -118,150 +119,6 @@ class ClassificacaoRiscoDetail extends TPage
         $paciente_id      ->addValidation( TextFormat::set( "Nome do Paciente" ), new TRequiredValidator );
         $dataclassificacao->addValidation( TextFormat::set( "Data da Avaliação" ), new TRequiredValidator );
 
-        $page2 = new TLabel( "Histórico Patológico", "#7D78B6", 12, "bi");
-        $page2->style="text-align:left;border-bottom:1px solid #c0c0c0;width:100%";
-        $this->form->appendPage( "Histórico Patológico" );
-        $this->form->addContent( [ $page2 ] );
-
-
-        /*--- frame de comorbidades ---*/
-        $frame1 = new TFrame;
-        $frame1->setLegend( "Comorbidades" );
-        $frame1->style .= ';margin:0px;width:95%';
-        $cid_id     = new THidden( "cid_id" );
-        $cid_codigo = new TDBSeekButton(
-            "cid_codigo", "database", "detail_classificacao_risco",
-            "VwCidRecord", "nomecid", "cid_id", "cid_codigo"
-        );
-        $add_button1 = TButton::create(
-            "add1", [ $this,"onError" ], null, null
-        );
-        $onSaveFrame1 = new TAction( [ $this, "onSaveFrames" ] );
-        $onSaveFrame1->setParameter( "fk", $fk );
-        $onSaveFrame1->setParameter( "did", $did );
-        $onSaveFrame1->setParameter( "frm", 1 );
-        $add_button1->setAction( $onSaveFrame1 );
-        $add_button1->setLabel( "Adicionar" );
-        $add_button1->setImage( "fa:plus green" );
-        $this->form->addContent( [ $frame1 ] );
-        $this->form->addField( $cid_codigo );
-        $this->form->addField( $add_button1 );
-        $this->framegrid1 = new TQuickGrid();
-        $this->framegrid1->setHeight(200);
-        $this->framegrid1->makeScrollable();
-        $this->framegrid1->style='width: 100%';
-        $this->framegrid1->id = 'framegrid1';
-        $this->framegrid1->disableDefaultClick();
-        $remove_action1 = new TDataGridAction( [ $this, "onDeleteFrames" ] );
-        $remove_action1->setParameter( "fk", $fk );
-        $remove_action1->setParameter( "did", $did );
-        $remove_action1->setParameter( "frm", 1 );
-        $this->framegrid1->addQuickAction( "Remover", $remove_action1, "id", "fa:trash red", "20%" );
-        $this->framegrid1->addQuickColumn( "Patologia", 'cid_codnome', 'left', '100%');
-        $this->framegrid1->createModel();
-        $hbox1 = new THBox;
-        $hbox1->add( $cid_codigo );
-        $hbox1->add( $add_button1 );
-        $hbox1->style = 'margin: 4px';
-        $vbox1 = new TVBox;
-        $vbox1->style='width:100%';
-        $vbox1->add( $hbox1 );
-        $vbox1->add( $this->framegrid1 );
-        $frame1->add( $vbox1 );
-        /*--------------------------------------*/
-
-        /*--- frame de uso de medicacoes ---*/
-        $frame2 = new TFrame;
-        $frame2->setLegend( "Uso de Medicações" );
-        $frame2->style .= ';margin:0px;width:95%';
-        $medicamento_id = new THidden( "medicamento_id" );
-        $medicamento_nome = new TDBSeekButton(
-            "medicamento_nome", "database", "detail_classificacao_risco",
-            "MedicamentoRecord", "nomemedicamento", "medicamento_id", "medicamento_nome"
-        );
-        $add_button2 = TButton::create(
-            "add2", [ $this,"onError" ], null, null
-        );
-        $onSaveFrame2 = new TAction( [ $this, "onSaveFrames" ] );
-        $onSaveFrame2->setParameter( "fk", $fk );
-        $onSaveFrame2->setParameter( "did", $did );
-        $onSaveFrame2->setParameter( "frm", 2 );
-        $add_button2->setAction( $onSaveFrame2 );
-        $add_button2->setLabel( "Adicionar" );
-        $add_button2->setImage( "fa:plus green" );
-        $this->form->addContent( [ $frame2 ] );
-        $this->form->addField( $medicamento_nome );
-        $this->form->addField( $add_button2 );
-        $this->framegrid2 = new TQuickGrid();
-        $this->framegrid2->setHeight(200);
-        $this->framegrid2->makeScrollable();
-        $this->framegrid2->style='width: 100%';
-        $this->framegrid2->id = 'framegrid2';
-        $this->framegrid2->disableDefaultClick();
-        $remove_action2 = new TDataGridAction( [ $this, "onDeleteFrames" ] );
-        $remove_action2->setParameter( "fk", $fk );
-        $remove_action2->setParameter( "did", $did );
-        $remove_action2->setParameter( "frm", 2 );
-        $this->framegrid2->addQuickAction( "Remover", $remove_action2, "id", "fa:trash red", "20%" );
-        $this->framegrid2->addQuickColumn( "Medicamento", 'medicamento_nome', 'left', '100%');
-        $this->framegrid2->createModel();
-        $hbox2 = new THBox;
-        $hbox2->add( $medicamento_nome );
-        $hbox2->add( $add_button2 );
-        $hbox2->style = 'margin: 4px';
-        $vbox2 = new TVBox;
-        $vbox2->style='width:100%';
-        $vbox2->add( $hbox2 );
-        $vbox2->add( $this->framegrid2 );
-        $frame2->add( $vbox2 );
-        /*--------------------------------------*/
-
-        /*--- frame de alergia medicamentosa ---*/
-        $frame3 = new TFrame;
-        $frame3->setLegend( "Alergia Medicamentosa" );
-        $frame3->style .= ';margin:0px;width:95%';
-        $principioativo_id = new THidden( "principioativo_id" );
-        $principioativo_nome = new TDBSeekButton(
-            "principioativo_nome", "database", "detail_classificacao_risco",
-            "PrincipioAtivoRecord", "nomeprincipioativo", "principioativo_id", "principioativo_nome"
-        );
-        $add_button3 = TButton::create(
-            "add3", [ $this,"onError" ], null, null
-        );
-        $onSaveFrame3 = new TAction( [ $this, "onSaveFrames" ] );
-        $onSaveFrame3->setParameter( "fk", $fk );
-        $onSaveFrame3->setParameter( "did", $did );
-        $onSaveFrame3->setParameter( "frm", 3 );
-        $add_button3->setAction( $onSaveFrame3 );
-        $add_button3->setLabel( "Adicionar" );
-        $add_button3->setImage( "fa:plus green" );
-        $this->form->addContent( [ $frame3 ] );
-        $this->form->addField( $principioativo_nome );
-        $this->form->addField( $add_button3 );
-        $this->framegrid3 = new TQuickGrid();
-        $this->framegrid3->setHeight(200);
-        $this->framegrid3->makeScrollable();
-        $this->framegrid3->style='width: 100%';
-        $this->framegrid3->id = 'framegrid3';
-        $this->framegrid3->disableDefaultClick();
-        $remove_action3 = new TDataGridAction( [ $this, "onDeleteFrames" ] );
-        $remove_action3->setParameter( "fk", $fk );
-        $remove_action3->setParameter( "did", $did );
-        $remove_action3->setParameter( "frm", 3 );
-        $this->framegrid3->addQuickAction( "Remover", $remove_action3, "id", "fa:trash red", "20%" );
-        $this->framegrid3->addQuickColumn( "Principio Ativo", 'principioativo_nome', 'left', '100%');
-        $this->framegrid3->createModel();
-        $hbox3 = new THBox;
-        $hbox3->add( $principioativo_nome );
-        $hbox3->add( $add_button3 );
-        $hbox3->style = 'margin: 4px';
-        $vbox3 = new TVBox;
-        $vbox3->style='width:100%';
-        $vbox3->add( $hbox3 );
-        $vbox3->add( $this->framegrid3 );
-        $frame3->add( $vbox3 );
-        /*--------------------------------------*/
-
         $page1 = new TLabel( "Sinais Vitais", "#7D78B6", 12, "bi");
         $page1->style="text-align:left;border-bottom:1px solid #c0c0c0;width:100%";
         $this->form->appendPage( "Sinais Vitais" );
@@ -285,13 +142,155 @@ class ClassificacaoRiscoDetail extends TPage
         $this->form->addFields( [ new TLabel( "Queixa Principal:" ) ], [ $queixaprincipal ] );
         $this->form->addFields( [ new TLabel( "Observações:" ) ], [ $observacoes ] );
         $this->form->addFields( [ new TLabel( "Classificação: {$redstar}" ) ], [ $tipoclassificacaorisco_id ] );
+
+        $page2 = new TLabel( "Histórico Patológico", "#7D78B6", 12, "bi");
+        $page2->style="text-align:left;border-bottom:1px solid #c0c0c0;width:100%";
+        $this->form->appendPage( "Histórico Patológico" );
+        $this->form->addContent( [ $page2 ] );
+
+        /*----------- frame de comorbidades -----------*/
+        $frame1 = new TFrame;
+        $frame1->setLegend( "Comorbidades" );
+        $frame1->style .= ';margin:0px;width:95%';
+        $cid_id     = new THidden( "cid_id" );
+        $cid_codigo = new TDBSeekButton(
+            "cid_codigo", "database", "detail_classificacao_risco",
+            "VwCidRecord", "nomecid", "cid_id", "cid_codigo"
+        );
+        $add_button1 = TButton::create(
+            "add1", [ $this,"onError" ], null, null
+        );
+        $onSaveFrame1 = new TAction( [ $this, "onSaveFrames" ] );
+        $onSaveFrame1->setParameter( "fk", $fk );
+        $onSaveFrame1->setParameter( "did", $did );
+        $onSaveFrame1->setParameter( "page", $page );
+        $onSaveFrame1->setParameter( "frm", 1 );
+        $add_button1->setAction( $onSaveFrame1 );
+        $add_button1->setLabel( "Adicionar" );
+        $add_button1->setImage( "fa:plus green" );
+        $this->form->addContent( [ $frame1 ] );
+        $this->form->addField( $cid_codigo );
+        $this->form->addField( $add_button1 );
+        $this->framegrid1 = new TQuickGrid();
+        $this->framegrid1->makeScrollable();
+        $this->framegrid1->style='width:100%;height:0%;';
+        $this->framegrid1->id = 'framegrid1';
+        $this->framegrid1->disableDefaultClick();
+        $remove_action1 = new TDataGridAction( [ $this, "onDeleteFrames" ] );
+        $remove_action1->setParameter( "fk", $fk );
+        $remove_action1->setParameter( "did", $did );
+        $remove_action1->setParameter( "page", $page );
+        $remove_action1->setParameter( "frm", 1 );
+        $this->framegrid1->addQuickAction( "Remover", $remove_action1, "id", "fa:trash red", "20%" );
+        $this->framegrid1->addQuickColumn( "Patologia", 'cid_codnome', 'left', '100%');
+        $this->framegrid1->createModel();
+        $hbox1 = new THBox;
+        $hbox1->add( $cid_codigo );
+        $hbox1->add( $add_button1 );
+        $hbox1->style = 'margin: 4px';
+        $vbox1 = new TVBox;
+        $vbox1->style='width:100%';
+        $vbox1->add( $hbox1 );
+        $vbox1->add( $this->framegrid1 );
+        $frame1->add( $vbox1 );
+        /*--------------------------------------*/
+        /*--- frame de uso de medicacoes -------*/
+        $frame2 = new TFrame;
+        $frame2->setLegend( "Uso de Medicações" );
+        $frame2->style .= ';margin:0px;width:95%';
+        $medicamento_id = new THidden( "medicamento_id" );
+        $medicamento_nome = new TDBSeekButton(
+            "medicamento_nome", "database", "detail_classificacao_risco",
+            "MedicamentoRecord", "nomemedicamento", "medicamento_id", "medicamento_nome"
+        );
+        $add_button2 = TButton::create(
+            "add2", [ $this,"onError" ], null, null
+        );
+        $onSaveFrame2 = new TAction( [ $this, "onSaveFrames" ] );
+        $onSaveFrame2->setParameter( "fk", $fk );
+        $onSaveFrame2->setParameter( "did", $did );
+        $onSaveFrame2->setParameter( "page", $page );
+        $onSaveFrame2->setParameter( "frm", 2 );
+        $add_button2->setAction( $onSaveFrame2 );
+        $add_button2->setLabel( "Adicionar" );
+        $add_button2->setImage( "fa:plus green" );
+        $this->form->addContent( [ $frame2 ] );
+        $this->form->addField( $medicamento_nome );
+        $this->form->addField( $add_button2 );
+        $this->framegrid2 = new TQuickGrid();
+        $this->framegrid2->makeScrollable();
+        $this->framegrid2->style='width:100%;height:0%;';
+        $this->framegrid2->id = 'framegrid2';
+        $this->framegrid2->disableDefaultClick();
+        $remove_action2 = new TDataGridAction( [ $this, "onDeleteFrames" ] );
+        $remove_action2->setParameter( "fk", $fk );
+        $remove_action2->setParameter( "did", $did );
+        $remove_action2->setParameter( "page", $page );
+        $remove_action2->setParameter( "frm", 2 );
+        $this->framegrid2->addQuickAction( "Remover", $remove_action2, "id", "fa:trash red", "20%" );
+        $this->framegrid2->addQuickColumn( "Medicamento", 'medicamento_nome', 'left', '100%');
+        $this->framegrid2->createModel();
+        $hbox2 = new THBox;
+        $hbox2->add( $medicamento_nome );
+        $hbox2->add( $add_button2 );
+        $hbox2->style = 'margin: 4px';
+        $vbox2 = new TVBox;
+        $vbox2->style='width:100%';
+        $vbox2->add( $hbox2 );
+        $vbox2->add( $this->framegrid2 );
+        $frame2->add( $vbox2 );
+        /*--------------------------------------*/
+        /*--- frame de alergia medicamentosa ---*/
+        $frame3 = new TFrame;
+        $frame3->setLegend( "Alergia Medicamentosa" );
+        $frame3->style .= ';margin:0px;width:95%';
+        $principioativo_id = new THidden( "principioativo_id" );
+        $principioativo_nome = new TDBSeekButton(
+            "principioativo_nome", "database", "detail_classificacao_risco",
+            "PrincipioAtivoRecord", "nomeprincipioativo", "principioativo_id", "principioativo_nome"
+        );
+        $add_button3 = TButton::create(
+            "add3", [ $this,"onError" ], null, null
+        );
+        $onSaveFrame3 = new TAction( [ $this, "onSaveFrames" ] );
+        $onSaveFrame3->setParameter( "fk", $fk );
+        $onSaveFrame3->setParameter( "did", $did );
+        $onSaveFrame3->setParameter( "page", $page );
+        $onSaveFrame3->setParameter( "frm", 3 );
+        $add_button3->setAction( $onSaveFrame3 );
+        $add_button3->setLabel( "Adicionar" );
+        $add_button3->setImage( "fa:plus green" );
+        $this->form->addContent( [ $frame3 ] );
+        $this->form->addField( $principioativo_nome );
+        $this->form->addField( $add_button3 );
+        $this->framegrid3 = new TQuickGrid();
+        $this->framegrid3->makeScrollable();
+        $this->framegrid3->style='width:100%;height:0%;';
+        $this->framegrid3->id = 'framegrid3';
+        $this->framegrid3->disableDefaultClick();
+        $remove_action3 = new TDataGridAction( [ $this, "onDeleteFrames" ] );
+        $remove_action3->setParameter( "fk", $fk );
+        $remove_action3->setParameter( "did", $did );
+        $remove_action3->setParameter( "page", $page );
+        $remove_action3->setParameter( "frm", 3 );
+        $this->framegrid3->addQuickAction( "Remover", $remove_action3, "id", "fa:trash red", "20%" );
+        $this->framegrid3->addQuickColumn( "Principio Ativo", 'principioativo_nome', 'left', '100%');
+        $this->framegrid3->createModel();
+        $hbox3 = new THBox;
+        $hbox3->add( $principioativo_nome );
+        $hbox3->add( $add_button3 );
+        $hbox3->style = 'margin: 4px';
+        $vbox3 = new TVBox;
+        $vbox3->style='width:100%';
+        $vbox3->add( $hbox3 );
+        $vbox3->add( $this->framegrid3 );
+        $frame3->add( $vbox3 );
+        /*--------------------------------------*/
         $this->form->addFields( [ $id, $paciente_id, $bau_id, $enfermeiro_id, $cid_id, $medicamento_id, $principioativo_id ] );
 
         $onSave = new TAction( [ $this, "onSave" ] );
         $onSave->setParameter( "fk", $fk );
         $onSave->setParameter( "did", $did );
-
-        $page = filter_input( INPUT_GET, "page" );
 
         switch ( $page ) {
 
@@ -436,13 +435,7 @@ class ClassificacaoRiscoDetail extends TPage
     public function onDelete( $param = null )
     {
         if( isset( $param[ "key" ] ) ) {
-/*
-            $param = [
-                "key" => $param[ "key" ],
-                "fk"  => $param[ "fk" ],
-                "did"  => $param[ "did" ]
-            ];
-*/
+
             $action1 = new TAction( [ $this, "Delete" ] );
             $action2 = new TAction( [ $this, "onReload" ] );
 
@@ -501,7 +494,7 @@ class ClassificacaoRiscoDetail extends TPage
 
             $objects = $repository->load( $criteria, FALSE );
 
-            if ( isset( $objects ) ) {
+            if ( !empty( $objects ) ) {
 
                 $this->datagrid->clear();
 
@@ -549,7 +542,7 @@ class ClassificacaoRiscoDetail extends TPage
 
             TTransaction::open( "database" );
 
-            if ( isset( $object ) ) {
+            if ( !empty( $object ) ) {
                 $object->store();
             } else {
                 $this->onError();
@@ -576,7 +569,7 @@ class ClassificacaoRiscoDetail extends TPage
 
             $object = $this->getFrameItem( $param );
 
-            if ( isset( $object ) ) {
+            if ( !empty( $object ) ) {
                 $object->delete();
             } else {
                 $this->onError();
@@ -603,7 +596,7 @@ class ClassificacaoRiscoDetail extends TPage
 
             $object = new PacienteRecord( $param[ "did" ] );
 
-            if ( isset( $object ) ) {
+            if ( !empty( $object ) ) {
 
                 foreach ( $object->getComorbidades() as $comorbidade ) {
                     $this->framegrid1->addItem( $comorbidade );
@@ -658,7 +651,7 @@ class ClassificacaoRiscoDetail extends TPage
 
         }
 
-        if ( isset( $object ) ) {
+        if ( !empty( $object ) ) {
 
             unset( $object->id );
             unset( $object->enfermeiro_id );
@@ -692,23 +685,27 @@ class ClassificacaoRiscoDetail extends TPage
 
     public function getFrameItem( $param = null )
     {
-        switch ( $param[ "frm" ] ) {
+        if ( isset( $param[ "frm" ] ) ) {
 
-            case 1:
-                $object = new BauComorbidadesRecord( $param[ "key" ] );
-                break;
+            switch ( $param[ "frm" ] ) {
 
-            case 2:
-                $object = new BauUsoMedicacoesRecord( $param[ "key" ] );
-                break;
+                case 1:
+                    $object = new BauComorbidadesRecord( $param[ "key" ] );
+                    break;
 
-            case 3:
-                $object = new BauAlergiaMedicamentosaRecord( $param[ "key" ] );
-                break;
+                case 2:
+                    $object = new BauUsoMedicacoesRecord( $param[ "key" ] );
+                    break;
+
+                case 3:
+                    $object = new BauAlergiaMedicamentosaRecord( $param[ "key" ] );
+                    break;
+
+            }
 
         }
 
-        return isset( $object ) ? $object : null;
+        return !empty( $object ) ? $object : null;
     }
 
     public function onError()
