337df34bf169589d381b0348dce7eb65fd139d0a
SAPE99\admin <geronimocpd@gmail.com>
2017-10-20 13:57:09 -0300

    Correção de busca

--- app/control/cadastros/PacientesAtendimentoList.class.php
+++ app/control/cadastros/PacientesAtendimentoList.class.php
@@ -1,35 +1,50 @@
 <?php
 
-class PacientesAtendimentoList extends TPage
+class PacientesAtendimentoList extends TStandardList
 {
-    private $form;
-    private $datagrid;
-    private $pageNavigation;
-    private $loaded;
+    protected $form;
+    protected $datagrid; 
+    protected $pageNavigation;
 
     public function __construct()
     {
         parent::__construct();
 
+        $criteria = new TCriteria();
+        $criteria->add(new TFilter('situacao', '=', 'CLASSIFICADO'));
+
+        parent::setDatabase('database');
+        parent::setActiveRecord('VwBauPacientesRecord');
+        parent::setCriteria($criteria);
+
+        parent::addFilterField('nomepaciente', 'like', 'nomepaciente');
+        //parent::addFilterField('dataentrada', 'like', 'nomepaciente');
+        //parent::addFilterField('dataentrada', '=', 'nomepaciente'); // filter field, operator, form field
+        
+        parent::setDefaultOrder('dataentrada', 'desc');
+        $this->setLimit(-1); // turn off limit for datagrid
+
         $this->form = new BootstrapFormBuilder( "list_pacientes_atendimento" );
         $this->form->setFormTitle( "Atendimento " );
         $this->form->class = "tform";
 
-        $opcao = new TCombo( "opcao" );
-        $dados = new TEntry( "dados" );
+        //$opcao = new TCombo( "opcao" );
+        $dados = new TEntry( "nomepaciente" );
 
-        $opcao->setDefaultOption( "..::SELECIONE::.." );
+        //$opcao->setDefaultOption( "..::SELECIONE::.." );
         $dados->setProperty ( "title", "Informe os dados referentes a opção" );
 
-        $opcao->setSize( "38%" );
+        //$opcao->setSize( "38%" );
         $dados->setSize( "38%" );
 
-        $opcao->addItems( [ "nomepaciente" => "Paciente" ] );
+        //$opcao->addItems( [ "nomepaciente" => "Paciente" ] );
 
-        $this->form->addFields( [ new TLabel( "Opção de busca:" ) ], [ $opcao ] );
+        //$this->form->addFields( [ new TLabel( "Opção de busca:" ) ], [ $opcao ] );
         $this->form->addFields( [ new TLabel( "Dados à buscar:" )  ], [ $dados ] );
 
         $this->form->addAction( "Buscar", new TAction( [ $this, "onSearch" ] ), "fa:search" );
+
+        //$this->form->setData( TSession::getValue('nomepaciente') );
 
         $this->datagrid = new BootstrapDatagridWrapper( new CustomDataGrid() );
         $this->datagrid->datatable = "true";
@@ -64,151 +79,13 @@ class PacientesAtendimentoList extends TPage
         $container = new TVBox();
         $container->style = "width: 90%";
         $container->add( $this->form );
+
+        //$container->add($this->datagrid);
         $container->add( TPanelGroup::pack( NULL, $this->datagrid ) );
         $container->add( $this->pageNavigation );
 
         parent::add( $container );
     }
-
-    public function onReload()
-    {
-        try {
-
-            TTransaction::open( "database" );
-
-            $repository = new TRepository( "VwBauPacientesRecord" );
-
-
-            $properties = [
-                "order" => "dataentrada",
-                "direction" => "asc"
-            ];
-
-            $limit = 10;
-
-            $criteria = new TCriteria();
-            $criteria->setProperties( $properties );
-            $criteria->setProperty( "limit", $limit );
-            $criteria->add(new TFilter('situacao', '=', 'CLASSIFICADO'));
-
-            $objects = $repository->load( $criteria, FALSE );
-
-            if ( isset( $objects ) ) {
-
-                $this->datagrid->clear();
-
-                foreach ( $objects as $object ) {
-
-                    //$dataentrada = new DateTime( $object->dataentrada );
-                    $horaentrada = new DateTime( $object->horaentrada );
-
-                    //$object->dataentrada = $dataentrada->format("d/m/Y");
-                    $object->horaentrada = $horaentrada->format("H:i");
-
-                    $this->datagrid->addItem( $object );
-                }
-
-            }
-
-            $criteria->resetProperties();
-
-            $count = $repository->count( $criteria );
-
-            $this->pageNavigation->setCount( $count );
-            $this->pageNavigation->setProperties( $properties );
-            $this->pageNavigation->setLimit( $limit );
-
-            TTransaction::close();
-
-            $this->loaded = true;
-
-        } catch ( Exception $ex ) {
-
-            TTransaction::rollback();
-
-            new TMessage( "error", $ex->getMessage() );
-        }
-    }
-
-
-    public function onSearch()
-    {
-        try {
-
-            if( !empty( $data->opcao ) && !empty( $data->dados ) ) {
-
-                TTransaction::open( "database" );
-                $repository = new TRepository( "VwBauPacientesRecord" );
-
-                if ( empty( $param[ "order" ] ) ) {
-                    $param[ "order" ] = "dataentrada";
-                    $param[ "direction" ] = "desc";
-                }
-
-                $limit = 10;
-
-                $criteria = new TCriteria();
-                $criteria->setProperties( $param );
-                $criteria->setProperty( "limit", $limit );
-                $criteria->add(new TFilter('situacao', '=', 'CLASSIFICADO'));
-                $criteria->add(new TFilter('situacao', '=', 'RETORNO'));
-
-                switch( $data->opcao ) {
-
-                    case "nomepaciente":
-
-                        $criteria->add( new TFilter( $data->opcao, "LIKE", $data->dados . "%" ) );
-
-                        break;
-
-                }
-
-
-                $objects = $repository->load( $criteria, FALSE );
-
-                $this->datagrid->clear();
-
-                if ( $objects ) {
-                    foreach ( $objects as $object ) {
-                        $dataentrada = new DateTime( $object->dataentrada );
-                        $horaentrada = new DateTime( $object->horaentrada );
-
-                        $object->dataentrada = $dataentrada->format("d/m/Y");
-                        $object->horaentrada = $horaentrada->format("H:i");
-                        $this->datagrid->addItem( $object );
-                    }
-                } else {
-                  new TMessage( "info", "Não há dados cadastrados!" );
-                }
-
-                $criteria->resetProperties();
-                $count = $repository->count( $criteria );
-
-                $this->pageNavigation->setCount( $count );
-                $this->pageNavigation->setProperties( $param );
-                $this->pageNavigation->setLimit( $limit );
-
-                TTransaction::close();
-                $this->form->setData( $data );
-                $this->loaded = true;
-
-            } else {
-
-                $this->onReload();
-                $this->form->setData( $data );
-                new TMessage( "erro", "Selecione uma opção e informe os dados à buscar corretamente!" );
-            }
-
-        } catch ( Exception $ex ) {
-
-            TTransaction::rollback();
-
-            $this->form->setData( $data );
-
-            new TMessage( "erro", $ex->getMessage() );
-        }
-    }
-
 
     public function show(){
 
